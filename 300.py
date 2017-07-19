20d5b4e718f159fbdccef27057124ffc5b84b7fd
Jarosalaw Ciolek-Zelechowski <zelechowski28@gmail.com>
2017-07-19 13:43:13 +0200

    Wypisze gorace o zadanym indeksie

--- 300.py
+++ 300.py
@@ -1,4 +1,5 @@
 import wykop
+from sys import argv
 from bs4 import BeautifulSoup
 
 klucz = "9dFENlI8xb"
@@ -7,21 +8,20 @@ sekret = "UH2uLO4KZH"
 api = wykop.WykopAPI(klucz, sekret)
 gorace = api.get_stream_hot()
 
-for i in range(len(gorace)):
-    index = i
+nazwa, liczba = argv
+index = int(liczba)
 
-    #zmienne:
-    TekstWyciagniety = gorace[index].body
+#zmienne:
+TekstWyciagniety = gorace[index].body
 
-    Zdjecie = None
-    if hasattr(gorace[index], "embed"):
-        if hasattr(gorace[index].embed, "url"):
-            Zdjecie = gorace[index].embed.url
+Zdjecie = None
+if hasattr(gorace[index], "embed"):
+    if hasattr(gorace[index].embed, "url"):
+        Zdjecie = gorace[index].embed.url
 
-    soup = BeautifulSoup(TekstWyciagniety, 'html.parser')
-    TekstPostu = soup.get_text()
+soup = BeautifulSoup(TekstWyciagniety, 'html.parser')
+TekstPostu = soup.get_text()
 
-    print(i, ": ----------------")
-    print (TekstPostu)
-    if (Zdjecie != None):
-        print (Zdjecie)
+print (TekstPostu)
+if (Zdjecie != None):
+    print (Zdjecie)
