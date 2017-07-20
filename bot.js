db20972d43f3105052cfd667050c478542ce7076
Jarosalaw Ciolek-Zelechowski <zelechowski28@gmail.com>
2017-07-20 13:09:24 +0200

    Pierwszy tweet!

--- bot.js
+++ bot.js
@@ -5,3 +5,17 @@ var Twit = require('twit');
 var config = require('./config');
 
 var T = new Twit(config);
+
+var tweet = {
+	status: '#FirstTime Pierwszy raz z wykorzystaniem API!'
+}
+
+T.post('statuses/update', tweet, tweeted);
+
+function tweeted(err, data, response){
+	if(err){
+		console.log("Nie działa :c");
+	}else{
+		console.log("Działa :)");
+	}
+}
