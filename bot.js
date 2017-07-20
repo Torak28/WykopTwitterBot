f90b6cc8b87d702abf7d9e4a0c793b080ddb7cdf
Jarosalaw Ciolek-Zelechowski <zelechowski28@gmail.com>
2017-07-20 16:17:48 +0200

    Dodanie followa

--- bot.js
+++ bot.js
@@ -1,21 +1,32 @@
 console.log("Lecimy");
 
 var Twit = require('twit');
-
 var config = require('./config');
-
 var T = new Twit(config);
 
-var tweet = {
-	status: '#FirstTime Pierwszy raz z wykorzystaniem API!'
+var stream = T.stream('user');
+
+function followed(eventMsg){
+	var nazwa = eventMsg.source.name;
+	var uzytkownik = eventMsg.source.screen_name;
+	tweetIt('Elo @' + uzytkownik + ' dzieki za folołka!');
 }
 
-T.post('statuses/update', tweet, tweeted);
+function tweetIt(txt){
+	var tweet = {
+		status: txt
+	}
 
-function tweeted(err, data, response){
-	if(err){
-		console.log("Nie działa :c");
-	}else{
-		console.log("Działa :)");
+	T.post('statuses/update', tweet, tweeted);
+
+	function tweeted(err, data, response){
+		if(err){
+			console.log("Nie działa :c");
+		}else{
+			console.log("Działa :)");
+		}
 	}
 }
+
+//setInterval(tweetIt, 1000*60*60);
+stream.om('follow', followed);
