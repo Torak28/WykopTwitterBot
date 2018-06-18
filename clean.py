8a16a50885a083298fc8e1890722bede6da277ec
Jarosław Ciołek-Żelechowski <zelechowski28@gmail.com>
2018-06-18 19:37:56 +0200

    Add some cleanup

--- clean.py
+++ clean.py
@@ -5,12 +5,16 @@ import shutil
 filenamePicture = "output.jpg"
 filenameText = "output.txt"
 pycahce = "./__pycache__"
+json = "data.json"
 
 if os.path.exists(filenamePicture):
 	os.remove(filenamePicture)
 
 if os.path.exists(filenameText):
 	os.remove(filenameText)
+
+if os.path.exists(json):
+	os.remove(json)
 
 if os.path.exists(pycahce):
 	shutil.rmtree(pycahce)
